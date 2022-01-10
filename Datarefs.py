135c33c40af3606c2ec4329cf337da4798af21b8
ShivaSoleimany <shiva.soleimany.dzch@gmail.com>
2022-01-09 20:49:46 -0700

    cleaned Datarefs.py

--- Datarefs.py
+++ Datarefs.py
@@ -59,61 +59,6 @@ def ex():
             return
     return roll, pitch
 
-    #     for i in range(100):
-            
-    #         posi = [37.524, -122.06899, 2500, 0,    0,   0,  1]
-    #         client.sendPOSI(posi)
-            
-
-    #         # Set angle of attack, velocity, and orientation using the DATA command
-    #         # print "Setting orientation"
-    #         data = [\
-    #             [18,   0, -998,   0, -998, -998, -998, -998, -998],\
-    #             [ 3, 130,  130, 130,  130, -998, -998, -998, -998],\
-    #             [16,   0,    0,   0, -998, -998, -998, -998, -998]\
-    #             ]
-    #         client.sendDATA(data)
-
-    #         # Set control surfaces and throttle of the player aircraft using sendCTRL
-    #         # print "Setting controls"
-    #         ctrl = [0.0, 0.0, 0.0, 0.8]
-    #         client.sendCTRL(ctrl)
-
-    #         # # Pause the sim
-    #         # print "Pausing"
-    #         # client.pauseSim(True)
-    #         # sleep(2)
-
-    #         # # Toggle pause state to resume
-    #         # print "Resuming"
-    #         # client.pauseSim(False)
-
-    #         # Stow landing gear using a dataref
-    #         # print "Stowing gear"
-    #         # gear_dref = "sim/cockpit/switches/gear_handle_status"
-    #         # client.sendDREF(gear_dref, 0)
-
-
-    #         # Let the sim run for a bit.
-    #         # sleep(4)
-
-    #         # Make sure gear was stowed successfully
-    #         # gear_status = client.getDREF(gear_dref)
-    #         roll.append(client.getDREF(roll_electric_dref)[0])
-    #         pitch.append(client.getDREF(pitch_electric_dref)[0])
-
-    #         # if gear_status[0] == 0:
-    #         #     print "Gear stowed"
-    #         # else:
-    #         #     print "Error stowing gear"
-
-    # print ("End of Python client example")
-    # raw_input("Press any key to exit...")
-
-
-    # return roll, pitch
-    # # print(roll)
-    # # print(pitch)
 
 if __name__ == "__main__":
     roll, pitch = ex()
